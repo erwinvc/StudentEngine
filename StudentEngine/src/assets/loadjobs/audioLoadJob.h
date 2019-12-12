07419084e8fc765ab62cdda7d185bf7c22518eeb
Brian Duncker <luguoslight@gmail.com>
2019-12-12 21:06:24 +0100

    Temporarily removes other audio related code as well.

--- StudentEngine/src/assets/loadjobs/audioLoadJob.h
+++ StudentEngine/src/assets/loadjobs/audioLoadJob.h
@@ -1,17 +1,1 @@
 #pragma once
-#define MINIMP3_IMPLEMENTATION
-
-class AudioLoadJob : public AssetLoadJob
-{
-private:
-	Path m_filePath;
-	AudioType m_type;
-	Audio* newAsset;
-	//int LoadMp3();
-	void LoadWav();
-public:
-	AudioLoadJob(const String& id, const Path& filePath, const AudioType& type);
-	~AudioLoadJob();
-	bool LoadAsset(bool addToProcessQueue = true) override;
-	void ProcessAsset(map<String, AssetBase*>& assets) override;
-};
